f9e90236d1d2cef6e5ef589dd471869382b4ea87
dokma11 <vule.dok@gmail.com>
2024-03-13 11:11:21 +0100

    refactor: Unnecessary comments removal

--- src/tours/repo/EquipmentRepository.go
+++ src/tours/repo/EquipmentRepository.go
@@ -20,28 +20,19 @@ func (repo *EquipmentRepository) GetById(id string) (model.Equipment, error) {
 }
 
 func (repo *EquipmentRepository) GetAll() ([]model.Equipment, error) {
-	//println("USAO ZA GET ALL: ")
 	var equipment []model.Equipment
 	dbResult := repo.DatabaseConnection.Find(&equipment)
-
-	/*for _, e := range equipment {
-		//println("Ime: " + e.Name)
-	}*/
-
 	if dbResult.Error != nil {
-		//println("EROR U EQUIPMENT REPO ZA GET ALL")
 		return nil, dbResult.Error
 	}
-
 	return equipment, nil
 }
 
 func (repo *EquipmentRepository) Create(review *model.Equipment) error {
-	//println("USAO ZA CREATE: ")
 	dbResult := repo.DatabaseConnection.Create(review)
 	if dbResult.Error != nil {
 		return dbResult.Error
 	}
-	//println("Rows affected: ", dbResult.RowsAffected)
+	println("Rows affected: ", dbResult.RowsAffected)
 	return nil
 }
