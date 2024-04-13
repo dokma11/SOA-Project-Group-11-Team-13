8ff55b22c647b2f5d9121b3c1b7dc62beff6090c
spasoje2001 <spasoje.brboric@gmail.com>
2024-04-13 14:24:55 +0200

    a lot of things, spale i veljo kraljevi

--- src/blogs/repo/CommentRepository.go
+++ src/blogs/repo/CommentRepository.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"blogs/model"
+	"errors"
 
 	"gorm.io/gorm"
 )
@@ -19,6 +20,23 @@ func (repo *CommentRepository) GetById(id string) (model.Comment, error) {
 	return blog, nil
 }
 
+func (repo *CommentRepository) GetByBlogId(id string, page int, pageSize int) ([]model.Comment, int, error) {
+	var comments []model.Comment
+	var totalCount int64
+
+	// First, get the total count of comments
+	repo.DatabaseConnection.Model(&model.Comment{}).Where("blog_id = ?", id).Count(&totalCount)
+
+	// Then, get the paginated list of comments
+	result := repo.DatabaseConnection.Where("blog_id = ?", id).Find(&comments)
+
+	if result.Error != nil {
+		return nil, 0, result.Error
+	}
+
+	return comments, int(totalCount), nil
+}
+
 func (repo *CommentRepository) GetAll() ([]model.Comment, error) {
 	var comments []model.Comment
 	dbResult := repo.DatabaseConnection.Find(&comments)
@@ -26,4 +44,38 @@ func (repo *CommentRepository) GetAll() ([]model.Comment, error) {
 		return nil, dbResult.Error
 	}
 	return comments, nil
+}
+
+func (repo *CommentRepository) Create(comment *model.Comment) error {
+	dbResult := repo.DatabaseConnection.Create(comment)
+	if dbResult.Error != nil {
+		return dbResult.Error
+	}
+	println("Rows affected: ", dbResult.RowsAffected)
+	return nil
+}
+
+func (repo *CommentRepository) Delete(id string) error {
+	dbResult := repo.DatabaseConnection.Where("id = ?", id).Delete(&model.Comment{})
+	if dbResult.Error != nil {
+		return dbResult.Error
+	}
+	if dbResult.RowsAffected == 0 {
+		return errors.New("no comment found for deletion")
+	}
+	return nil
+}
+
+func (repo *CommentRepository) Update(comment *model.Comment) error {
+	println(comment)
+	dbResult := repo.DatabaseConnection.Model(&model.Comment{}).Where("id = ?", comment.ID).Updates(comment)
+	if dbResult.Error != nil {
+		println("error se pojavio u bazi")
+		return dbResult.Error
+	}
+	if dbResult.RowsAffected == 0 {
+		println("Nista nije promenjeno")
+		return errors.New("no key point found for update")
+	}
+	return nil
 }
