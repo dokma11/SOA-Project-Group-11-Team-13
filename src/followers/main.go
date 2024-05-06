145b7815691e2cd548b5bbd2b8bbc1034e59978c
dokma11 <vule.dok@gmail.com>
2024-05-06 09:43:45 +0200

    feat: Switch from http to rpc

--- src/followers/main.go
+++ src/followers/main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"context"
 	"followers/handler"
+	"followers/proto/followers"
 	"followers/repo"
 	"followers/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
-	"net/http"
+	"net"
 	"os"
 	"os/signal"
 	"time"
-
-	gorillaHandlers "github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "8084"
-	}
+	//port := os.Getenv("PORT")
+	//if len(port) == 0 {
+	//	port = "8084"
+	//}
 
 	timeoutContext, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
@@ -34,51 +34,46 @@ func main() {
 	defer store.CloseDriverConnection(timeoutContext)
 	store.CheckConnection()
 
+	listener, err := net.Listen("tcp", "followers:8084")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(listener)
+
 	userService := service.NewUserService(logger, store)
 
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	logger.Println("Server starting with rpc")
+
 	userHandler := handler.NewUserHandler(logger, userService)
+	followers.RegisterFollowersServiceServer(grpcServer, userHandler)
 
-	router := mux.NewRouter().StrictSlash(true)
+	//cors := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}), gorillaHandlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}))
+	//server := http.Server{
+	//	Addr: ":" + port,
+	//	//Handler:      cors(router),
+	//	IdleTimeout:  120 * time.Second,
+	//	ReadTimeout:  5 * time.Second,
+	//	WriteTimeout: 5 * time.Second,
+	//}
 
-	router.Use(userHandler.MiddlewareContentTypeSet)
-
-	postUserNode := router.Methods(http.MethodPost).Subrouter()
-	postUserNode.HandleFunc("/users", userHandler.Create)
-	postUserNode.Use(userHandler.MiddlewareUserDeserialization)
-
-	postUserFollowNode := router.Methods(http.MethodPost).Subrouter()
-	postUserFollowNode.HandleFunc("/users/follow/{userId}/{followedById}", userHandler.Follow)
-
-	getNode := router.Methods(http.MethodGet).Subrouter()
-	getNode.HandleFunc("/users/{username}", userHandler.GetByUsername).Methods("GET")
-
-	getFollowersNode := router.Methods(http.MethodGet).Subrouter()
-	getFollowersNode.HandleFunc("/users/followers/{id}", userHandler.GetFollowers)
-
-	getFollowingsNode := router.Methods(http.MethodGet).Subrouter()
-	getFollowingsNode.HandleFunc("/users/followings/{id}", userHandler.GetFollowings)
-
-	getRecommendedNode := router.Methods(http.MethodGet).Subrouter()
-	getRecommendedNode.HandleFunc("/users/recommended/{id}", userHandler.GetRecommendedUsers)
-
-	deleteNode := router.Methods(http.MethodDelete).Subrouter()
-	deleteNode.HandleFunc("/users/unfollow/{followerId}/{followingId}", userHandler.Unfollow)
-
-	cors := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}), gorillaHandlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}))
-	server := http.Server{
-		Addr:         ":" + port,
-		Handler:      cors(router),
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-	}
-
-	logger.Println("Server starting", port)
-	logger.Println("Server listening on port", port)
+	//logger.Println("Server starting", port)
+	//logger.Println("Server listening on port", port)
 	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			logger.Fatal(err)
+		//err := server.ListenAndServe()
+		//if err != nil {
+		//	logger.Fatal(err)
+		//}
+
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatal("server error: ", err)
 		}
 	}()
 
@@ -89,8 +84,10 @@ func main() {
 	sig := <-sigCh
 	logger.Println("Received terminate, graceful shutdown", sig)
 
-	if server.Shutdown(timeoutContext) != nil {
-		logger.Fatal("Cannot gracefully shutdown...")
-	}
-	logger.Println("Server stopped")
+	//if server.Shutdown(timeoutContext) != nil {
+	//	logger.Fatal("Cannot gracefully shutdown...")
+	//}
+	//logger.Println("Server stopped")
+	grpcServer.Stop()
+
 }
