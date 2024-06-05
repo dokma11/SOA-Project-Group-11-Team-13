989421d9d830ffc171f42b29cdefb4c9e0d65a89
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2024-06-05 23:30:04 +0200

    Fixed docker

--- src/tour-search/src/main/java/com/soa/tour_search/controller/TourController.java
+++ src/tour-search/src/main/java/com/soa/tour_search/controller/TourController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping(value = "tours")
 @RequiredArgsConstructor
+@CrossOrigin
 public class TourController {
 
     private final ITourService tourService;
